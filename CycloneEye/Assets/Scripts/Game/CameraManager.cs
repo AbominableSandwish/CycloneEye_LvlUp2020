32cee54391c1619160c99c25a51ee7ff3ec52d7c
Sandwish <dylan_vonarx@hotmail.fr>
2020-02-14 11:22:24 +0100

    Added Averaging Position System Camera

--- CycloneEye/Assets/Scripts/Game/CameraManager.cs
+++ CycloneEye/Assets/Scripts/Game/CameraManager.cs
@@ -1,14 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] float shakeStrength = 0.1f;
+    private Vector3 positionOffSet;
 
     void Start()
     {
+        positionOffSet = transform.position;
         EventManager.onPlayerDamaged.AddListener(Chake);
+
+        this.players = new List<Transform>();
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var player in players)
+        {
+            print(player.name);
+            this.players.Add(player.GetComponent<Transform>());
+        }
+
+    }
+
+    private List<Transform> players;
+
+
+    void FixedUpdate()
+    {
+        Debug.Log(PositionAveraging().magnitude);
+        transform.position = positionOffSet + Vector3.down * PositionAveraging().magnitude;
+    }
+
+    Vector3 PositionAveraging()
+    {
+        Vector3 average = Vector3.zero;
+        foreach (var player in this.players)
+        {
+            Debug.Log(player.localPosition);
+            average += player.localPosition;
+        }
+
+        average /= this.players.Count;
+        return average;
+    }
+
+    Vector3 LerpSmoothing()
+    {
+        return new Vector3();
     }
 
     void Chake()
