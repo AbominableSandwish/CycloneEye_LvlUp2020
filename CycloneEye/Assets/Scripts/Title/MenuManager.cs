2236518fc7e2b26a41346406870f5464536ac149
LAPTOP-JAGF2RL6\issab <issa.barbier@gmail.com>
2020-02-14 15:07:33 +0100

    progress Animated Main Menu + controller ui controls

--- CycloneEye/Assets/Scripts/Title/MenuManager.cs
+++ CycloneEye/Assets/Scripts/Title/MenuManager.cs
@@ -7,15 +7,23 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] BlackPanel blackPanel;
+    [SerializeField] GameObject[] menuCursors;
+
+    [SerializeField] GameObject settingsPanel;
+    [SerializeField] GameObject gamePanel;
 
     [SerializeField] Slider numberTurnSlider;
     [SerializeField] Slider timerSlider;
 
     bool initializing = false;
+    int index = 0;
+    float timer = 0;
+    bool menuActif = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         blackPanel.Hide();
         initializing = true;
         numberTurnSlider.value = GameManager.maxRund;
@@ -24,10 +32,55 @@ public class MenuManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!gamePanel.activeSelf && !settingsPanel.activeSelf && menuActif)
+        {
+            timer -= Time.deltaTime;
+            if (Input.GetAxis("Vertical") > 0 && index > 0 && timer <= 0)
+            {
+                timer = 0.3f;
+                index--;
+                UpdateCursors();
+            }
+            else if (Input.GetAxis("Vertical") < 0 && index < menuCursors.Length - 1 && timer <= 0)
+            {
+                timer = 0.3f;
+                index++;
+                UpdateCursors();
+            }
+            else if (Input.GetButtonDown("Attack"))
+            {
+                StartCoroutine(ActivatedMenu());
+            }
+        }
+    }
+
+    IEnumerator ActivatedMenu()
+    {
+        menuCursors[index].GetComponent<Animator>().SetTrigger("activate");
+        yield return new WaitForSeconds(0.5f);
+        switch (index)
+        {
+            case 0:
+                gamePanel.SetActive(true);
+                break;
+            case 1:
+                settingsPanel.SetActive(true);
+                break;
+            case 2:
+                menuActif = false;
+                QuitGame();
+                break;
+        }
+    }
+
+    void UpdateCursors()
+    {
+        for(int i = 0; i < menuCursors.Length; i++)
+        {
+            menuCursors[i].GetComponent<Image>().enabled = (i == index);
+        }   
     }
 
     public void Play()
